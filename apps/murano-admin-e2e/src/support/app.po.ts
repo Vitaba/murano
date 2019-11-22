de040d03bc7052fc2e7c715bf46b707dd5f7e22b
Sebastián Guevara <rlxsebas@gmail.com>
2019-11-22 13:12:55 -0500

    feat(repo): adding e2e tests with testcafe (#3)

--- apps/murano-admin-e2e/src/support/app.po.ts
+++ apps/murano-admin-e2e/src/support/app.po.ts
@@ -1,1 +1,9 @@
-export const getGreeting = () => cy.get('h1');
+import { Selector } from 'testcafe';
+
+export class HomePage {
+  public title = Selector('h2');
+  public toobar = Selector('div.toolbar');
+  public newComponentButton = Selector('.e2e-new-component-button');
+  public angularMaterialButton = Selector('.e2e-angular-material-button');
+  public terminal = Selector('div.terminal');
+}
