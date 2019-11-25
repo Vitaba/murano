7dc8d2d3a02e82c6bd50fdd0aa25ac4ad4d5ecfe
Sebastian Guevara <rlxsebas@gmail.com>
2019-11-25 09:41:51 -0500

    feat(common): adding scss variables

--- libs/common/ui/src/lib/card-list/card-list.component.ts
+++ libs/common/ui/src/lib/card-list/card-list.component.ts
@@ -8,7 +8,10 @@ import { Items } from '@vitaba/common-utils';
 })
 export class CardListComponent {
   @Input() public title = 'Card List Example';
-  @Input() public styleClass = 'red';
+  @Input() public styles = {
+  title: 'font-sans text-lg text-gray-800 text-center mb-3',
+  items: 'bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded'
+  };
   @Input()
   public items: Items[] = [
     {
