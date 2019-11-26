9cae9ce4a3da17624f47fdf5e184ad1e1d293eb7
Sebastián Guevara <rlxsebas@gmail.com>
2019-11-25 21:32:11 -0500

    feat(common): add storybook for list-component (#6)

---
+++ apps/common-ui-e2e/src/integration/card-list/card-list.component.spec.ts
@@ -0,0 +1,11 @@
+import { Selector } from 'testcafe';
+
+const header = Selector('.sample');
+
+fixture('Home 1')
+ .page('http://localhost:4400/iframe.html?id=cardlistcomponent--primary&knob-items=[{%22name%22:%22a%22,%22value%22:%22a%22}]');
+
+test('Card List Works', async t => {
+  await t
+        .expect(header.textContent).contains('Sample');
+});
--- libs/common/ui/src/lib/card-list/card-list.component.ts
+++ libs/common/ui/src/lib/card-list/card-list.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Items } from '@vitaba/common-utils';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Items as Item } from '@vitaba/common-utils';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'vitaba-card-list',
@@ -7,7 +7,13 @@ import { Items } from '@vitaba/common-utils';
   templateUrl: './card-list.component.html',
 })
 export class CardListComponent {
-  public items: Items[] = [
+  @Input() public title = 'Card List Example';
+  @Input() public styles = {
+    items: 'bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded',
+    title: 'font-sans text-lg text-gray-800 text-center mb-3',
+  };
+  @Input()
+  public items: Item[] = [
     {
       name: 'Item 1',
       value: 1,
@@ -17,4 +23,8 @@ export class CardListComponent {
       value: 2,
     },
   ];
+
+  public trackByFn(option: Item) {
+    return option.value;
+  }
 }
