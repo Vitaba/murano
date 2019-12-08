9814c86bfbeea986611fd3669453b43f44cae806
Sebastian Guevara <rlxsebas@gmail.com>
2019-12-08 12:52:58 -0500

    chore(repo): adding cd

--- tools/scripts/package.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build
-echo "Building All Libraries..."
-echo ./node_modules/.bin/nx affected:build --all --parallel 
-echo "Vitaba libraries available at dist/libs:"
-ls dist/libs
--- tools/scripts/release.sh
+++ tools/scripts/release.sh
@@ -1,1 +1,58 @@
-.node_modules/standard-version --prerelease beta
+# 
+COMMIT_MESSAGE=$(git log -1 --pretty=format:'%s')
+## https://stackoverflow.com/questions/19482123/extract-part-of-a-string-using-bash-cut-split
+COMMIT_TYPE=${COMMIT_MESSAGE%(*}  # retain the part before the colon
+COMMIT_SCOPE=${COMMIT_MESSAGE#*(}
+COMMIT_SCOPE=${COMMIT_SCOPE%)*}
+
+case "release" in
+'release')
+    ENV="prod";
+    echo "Release for production 👌"
+    ;;
+*)
+'pre-release')
+    ENV="beta";
+    COMMIT_SCOPE="rc"
+    echo "Pre-release for production 👌"
+    ;;
+*)
+echo 'default'
+    ENV="beta"
+    COMMIT_SCOPE="beta"
+    echo "Release for beta 👌"
+    ;;
+
+esac
+
+echo ${ENV}
+
+case "$COMMIT_TYPE" in
+'feat')
+    echo "👉️ Major Release "
+    if $ENV = "prod" then
+    .node_modules/bin/release-it --ci major;
+    else 
+    .node_modules/bin/release-it --ci major --preRelease=COMMIT_SCOPE;
+    fi;
+    ;;
+'refactor'| 'test')
+    echo "👉️ minor release"
+    if $ENV = "prod" then
+    .node_modules/bin/release-it --ci minor;
+    else 
+    .node_modules/bin/release-it --ci minor --preRelease=COMMIT_SCOPE;
+    fi;
+    ;;    
+'chore' | 'build' | 'fix' | 'style' | 'docs')
+    echo "👉️ patch release"
+    if $ENV = "prod" then
+    .node_modules/bin/release-it --ci patch;
+    else 
+    .node_modules/bin/release-it --ci patch --preRelease=COMMIT_SCOPE;
+    fi;
+    ;;
+esac
+
+.node_modules/bin/nx affected --target=build --all;
+.node_modules/bin/nx affected --target=deploy --all;
