df7fefea0be696ce00d666772559fb8b4957da33
Sebastian Guevara <rlxsebas@gmail.com>
2019-12-10 11:12:18 -0500

    Release 5.0.1-beta.0

--- tools/scripts/release.sh
+++ tools/scripts/release.sh
@@ -2,17 +2,16 @@
 # echo "👉️ minor release"
 #     if [ "prod" = "prod" ]; then echo "equal";
 #     else echo "not equal";fi;
-echo "👻 Building libraries for release"
-node_modules/.bin/nx affected:build --all
-echo "👻 Building apps for release"
-node_modules/.bin/nx affected --target=package --configuration=production --all --parallel 
+# echo "👻 Building libraries for release"
+# node_modules/.bin/nx affected:build --all
+# echo "👻 Building apps for release"
+# node_modules/.bin/nx affected --target=package --configuration=production --all --parallel 
 
 COMMIT_MESSAGE=$(git log -1 --pretty=format:'%s')
 ## https://stackoverflow.com/questions/19482123/extract-part-of-a-string-using-bash-cut-split
 COMMIT_TYPE=${COMMIT_MESSAGE%(*}  # retain the part before the colon
 COMMIT_SCOPE=${COMMIT_MESSAGE#*(}
 COMMIT_SCOPE=${COMMIT_SCOPE%)*}
-
 # Based on feat(release): adding new feature
 case "${COMMIT_SCOPE}" in
 'release')
@@ -51,13 +50,13 @@ case "$COMMIT_TYPE" in
 'chore' | 'build' | 'fix' | 'style' | 'docs')
     echo "👉️ patch release";
     if  [ "$ENV" = "prod" ]; then
-    ./node_modules/.bin/release-it --ci patch;
+    ./node_modules/.bin/release-it --no-git.requireCleanWorkingDir --no-git.requireUpstream --ci patch;
     else 
-    ./node_modules/.bin/release-it --ci patch --preRelease=$COMMIT_SCOPE;
+    ./node_modules/.bin/release-it --no-git.requireCleanWorkingDir --no-git.requireUpstream --ci patch --preRelease=$COMMIT_SCOPE;
     fi;
     ;;
 esac
 
-firebase deploy --only hosting:$ENV --non-interactive --token "$FIREBASE_TOKEN";
-./node_modules/.bin/nx affected --target=deploy --all --parallel
+# firebase deploy --only hosting:$ENV --non-interactive --token "$FIREBASE_TOKEN";
+# ./node_modules/.bin/nx affected --target=deploy --all --parallel
 
