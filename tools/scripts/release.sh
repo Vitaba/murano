bbb4c7084573aad3091e82ca565a801f24e9ab2a
SebasG22 <rlxsebas@gmail.com>
2019-12-12 02:18:53 -0500

    chore(repo): update

--- tools/scripts/release.sh
+++ tools/scripts/release.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-cat <<-EOF
-$NPM_USER
-$NPM_PASS
-$NPM_EMAIL
-EOF | npm adduser
-cat $HOME/.npmrc
-npm whoami
-yarn install --network-timeout 1000000 --frozen-lockfile
-echo "👻 Building libraries for release"
-node_modules/.bin/nx affected:build --all
-npm whoami
-./node_modules/.bin/nx affected --target=deploy --all --parallel
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+npmwhoami
+
