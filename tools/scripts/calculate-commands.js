9d064991aedcc8dcd6f9611bc8938ef4c6c16fcc
SebasG22 <rlxsebas@gmail.com>
2019-12-16 15:56:56 -0500

    chore(repo): fix circle ci config

--- tools/scripts/calculate-commands.js
+++ tools/scripts/calculate-commands.js
@@ -10,7 +10,7 @@ console.log(
     ...commands('lint'),
     ...commands('test'),
     ...commands('build'),
-    // ...commands('e2e'),
+    ...commands('e2e'),
     ...commands('deploy'),
   })
 );
