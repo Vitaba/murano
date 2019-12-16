3f88fd52bfb68f5fd3921cae3a7de2efccd77d2c
SebasG22 <rlxsebas@gmail.com>
2019-12-16 15:12:09 -0500

    chore(repo): fix circle ci config

--- tools/scripts/run-many.js
+++ tools/scripts/run-many.js
@@ -5,11 +5,14 @@ const commands = JSON.parse(process.argv[2]);;
 const projects = commands[process.argv[3]];
 const target = process.argv[4];
 
-execSync(
-  `yarn nx run-many --target=${target} --projects=${projects.join(
-    ','
-  )} --parallel`,
-  {
-    stdio: [0, 1, 2]
-  }
-);
+// Validate not empty execute nothing when projects are empty
+if(projects.length > 0){
+  execSync(
+    `yarn nx run-many --target=${target} --projects=${projects.join(
+      ','
+    )} --parallel`,
+    {
+      stdio: [0, 1, 2]
+    }
+  );
+}
