da3f6bbc5458162537225b98bb47c576f2dff9f8
SebasG22 <rlxsebas@gmail.com>
2019-12-18 08:38:26 -0500

    chore(repo): fix circle ci config

--- tools/scripts/release-message.sh
+++ tools/scripts/release-message.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Current commit message"
-GIT_COMMIT_DESC=git log --format=%B -n 1 $CIRCLE_BRANCH
+GIT_COMMIT_DESC=$(git log --format=%B -n 1 $CIRCLE_BRANCH)
 echo $GIT_COMMIT_DESC
 if [[ ($GIT_COMMIT_DESC != *"Merge"*) && ($GIT_COMMIT_DESC != *"chore(bump):"*) ]]; then
 GENERATE_RELEASE=true;
 echo "not contains chore(bump) or merge commit";
-circleci-agent step halt;
 else
 GENERATE_RELEASE=false;
+circleci-agent step halt;
 echo "contains chore(bump) or merge commit";
 fi;
