7c9b7d873500e01404e369fe786bafcf017c9aec
Sebastián Guevara <rlxsebas@gmail.com>
2019-12-18 10:22:25 -0500

    chore(repo): fix circle ci config (#31)

--- tools/scripts/calculate-commands.js
+++ tools/scripts/calculate-commands.js
@@ -11,30 +11,46 @@ console.log(
     ...commands('test'),
     ...commands('build'),
     ...commands('e2e'),
-    ...commands('deploy'),
+    ...commands('deploy')
   })
 );
 
 function commands(target) {
-  const affected = execSync(
-    `yarn nx print-affected --base=${baseSha} --head=${head} --target=${target}`
-  ).toString();
-  
-    const array = JSON.parse(affected
-    .trim()
-    .replace(/\r?\n?[^\r\n]*$/, '')
-      .substring(affected.indexOf("\n") + 1)
-      .replace(`$ nx print-affected --base=${baseSha} --head=${head} --target=${target}`, "")
-      .trim()).tasks.map(t => t.target.project);
+  let affected;
+  switch (target) {
+    case 'build':
+    case 'deploy':
+      affected = execSync(
+        `yarn nx print-affected --base=${baseSha} --head=${head} --target=${target} --all`
+      ).toString();
+      break;
+    default:
+      affected = execSync(
+        `yarn nx print-affected --base=${baseSha} --head=${head} --target=${target}`
+      ).toString();
+      break;
+  }
 
-    array.sort(() => 0.5 - Math.random());
-    const third = Math.floor(array.length / 3);
-    const a1 = array.slice(0, third);
-    const a2 = array.slice(third, third * 2);
-    const a3 = array.slice(third * 2);
-    return {
-      [target + '1']: a1,
-      [target + '2']: a2,
-      [target + '3']: a3
-    };
+  const array = JSON.parse(
+    affected
+      .trim()
+      .replace(/\r?\n?[^\r\n]*$/, '')
+      .substring(affected.indexOf('\n') + 1)
+      .replace(
+        `$ nx print-affected --base=${baseSha} --head=${head} --target=${target}`,
+        ''
+      )
+      .trim()
+  ).tasks.map(t => t.target.project);
+
+  array.sort(() => 0.5 - Math.random());
+  const third = Math.floor(array.length / 3);
+  const a1 = array.slice(0, third);
+  const a2 = array.slice(third, third * 2);
+  const a3 = array.slice(third * 2);
+  return {
+    [target + '1']: a1,
+    [target + '2']: a2,
+    [target + '3']: a3
+  };
 }
