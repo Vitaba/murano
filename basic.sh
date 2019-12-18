17db841f563a2824b8480fafe43cbf65c3937d68
Sebastián Guevara <rlxsebas@gmail.com>
2019-12-18 08:42:11 -0500

    chore(repo): fix circle ci config (#30)

--- basic.sh
+++
@@ -1,2 +0,0 @@
-echo "TASKS=$(node ./tools/scripts/calculate-commands.js false)"
-echo $TASKS
--- tools/scripts/interactive_program.sh
+++
@@ -1,6 +0,0 @@
-echo "What is your name?"
-read ANSWER
-echo "How many dogs you have?"
-read ANSWER
-echo "How many cats you have?"
-read ANSWER
