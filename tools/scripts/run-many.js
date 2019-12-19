f29d41cb49fc7ee28b2616c161e0dce6201cb9f2
SebasG22 <rlxsebas@gmail.com>
2019-12-18 21:52:58 -0500

    chore(repo): fix circle ci config

--- tools/scripts/run-many.js
+++ tools/scripts/run-many.js
@@ -1,6 +1,6 @@
 const execSync = require('child_process').execSync;
 
-const commands = process.argv[2];
+const commands = JSON.parse(process.argv[2]);
 const projects = commands[process.argv[3]];
 const target = process.argv[4];
 
