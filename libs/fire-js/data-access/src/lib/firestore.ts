327452dbc438c87efa27deb5b3872160922419b7
Sebastián Guevara <rlxsebas@gmail.com>
2020-01-13 17:32:15 -0500

    chore(fire-js): support multiple queries (#64)

--- libs/fire-js/data-access/src/lib/firestore.ts
+++ libs/fire-js/data-access/src/lib/firestore.ts
@@ -5,14 +5,12 @@ let firestore: any;
 
 function uuid() {
   // tslint:disable-next-line: restrict-plus-operands
-  return (([1e7] as any) + 1e3 + 4e3 + 8e3).replace(
-    /[018]/g,
-    (c: any) =>
-      // tslint:disable: no-bitwise
-      (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16),
+  return (([1e7] as any) + 1e3 + 4e3 + 8e3).replace(/[018]/g, (c: any) =>
+    // tslint:disable: no-bitwise
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16),
   );
 }
 
@@ -34,39 +32,77 @@ export const workerFirestore = {
     const uid = uuid();
     const restaurantsCol = firestore.collection(collection);
     const snapshotRef = restaurantsCol.onSnapshot(snap => {
-        // unwrap the data from the snapshot
+      // unwrap the data from the snapshot
       callback({ uid, data: snap.docs.map(d => d.data()) });
     });
     subscribers.push({ uid, ref: snapshotRef });
   },
   getCollectionDocument(collection, document, callback, _error) {
     const uid = uuid();
-    const restaurantsCol = firestore.collection(collection)
-    .doc(document);
+    const restaurantsCol = firestore.collection(collection).doc(document);
     const snapshotRef = restaurantsCol.onSnapshot(snap => {
-        // unwrap the data from the snapshot
+      // unwrap the data from the snapshot
       callback({ uid, data: snap.data() });
     });
     subscribers.push({ uid, ref: snapshotRef });
   },
-  getSubCollection(collection, document, subcollection, callback, _error) {
+  getSubCollection(
+    collection, document, subcollection, limitValue, callback, _error) {
     const uid = uuid();
-    const restaurantsCol = firestore.collection(collection)
-    .doc(document).collection(subcollection);
+    const restaurantsCol = firestore
+      .collection(collection)
+      .doc(document)
+      .collection(subcollection).limit(limitValue);
     const snapshotRef = restaurantsCol.onSnapshot(snap => {
-        // unwrap the data from the snapshot
+      // unwrap the data from the snapshot
       callback({ uid, data: snap.docs.map(d => d.data()) });
     });
     subscribers.push({ uid, ref: snapshotRef });
   },
   filterSubCollectionByField(
-    collection, document, subcollection, field,
-    value, limitValue, callback, _error) {
+    collection,
+    document,
+    subcollection,
+    field,
+    value,
+    limitValue,
+    filters: Array<{
+      field: string;
+      // tslint:disable-next-line: ter-max-len
+      operator:
+        | '<'
+        | '<='
+        | '=='
+        | '>'
+        | '>='
+        | 'array-contains'
+        | 'in'
+        | 'array-contains-any';
+      value: any;
+    }>,
+    callback,
+    _error,
+  ) {
     const uid = uuid();
-    const restaurantsCol = firestore.collection(collection)
-    .doc(document).collection(subcollection)
-    // tslint:disable-next-line: restrict-plus-operands
-    .orderBy(field).startAt(value).endAt(value + '\uf8ff').limit(limitValue);
+    let restaurantsCol = firestore
+      .collection(collection)
+      .doc(document)
+      .collection(subcollection)
+      .orderBy(field)
+      .startAt(value)
+      // tslint:disable-next-line: restrict-plus-operands
+      .endAt(value + '\uf8ff')
+      .limit(limitValue);
+      // .where('hidden','==', false);
+    if (filters) {
+      filters.forEach(filter => {
+        restaurantsCol = restaurantsCol.where(
+          filter.field,
+          filter.operator,
+          filter.value,
+        );
+      });
+    }
     const snapshotRef = restaurantsCol.onSnapshot(snap => {
       // unwrap the data from the snapshot
       callback({ uid, data: snap.docs.map(d => d.data()) });
@@ -74,56 +110,100 @@ export const workerFirestore = {
     subscribers.push({ uid, ref: snapshotRef });
   },
   getSubCollectionDocument(
-    collection, collectionDoc, subcollection, subcollectionDoc,
-    callback, _error) {
+    collection,
+    collectionDoc,
+    subcollection,
+    subcollectionDoc,
+    callback,
+    _error,
+  ) {
     const uid = uuid();
-    const restaurantsCol = firestore.collection(collection)
-    .doc(collectionDoc).collection(subcollection).doc(subcollectionDoc);
+    const restaurantsCol = firestore
+      .collection(collection)
+      .doc(collectionDoc)
+      .collection(subcollection)
+      .doc(subcollectionDoc);
     const snapshotRef = restaurantsCol.onSnapshot(snap => {
-        // unwrap the data from the snapshot
+      // unwrap the data from the snapshot
       callback({ uid, data: snap.data() });
     });
     subscribers.push({ uid, ref: snapshotRef });
   },
   addDocumentToSubCollection(
-    collection, document, subcollection, value, callback, _error) {
+    collection,
+    document,
+    subcollection,
+    value,
+    callback,
+    _error,
+  ) {
     // TODO: Validate the object exists before making the set
     workerFirestore.setDocumentToSubCollection(
-      collection, document,
+      collection,
+      document,
       uuid(),
-      subcollection, {
+      subcollection,
+      {
         added_at: firebase.firestore.FieldValue.serverTimestamp(),
         hidden: false,
-        ...value },
-      callback, _error);
+        ...value,
+      },
+      callback,
+      _error,
+    );
   },
   deleteDocumentToSubCollection(
-    collection, document, subcollection, docUID, callback, _error) {
+    collection,
+    document,
+    subcollection,
+    docUID,
+    callback,
+    _error,
+  ) {
     const uid = uuid();
-      // TODO: Validate the object exists before making the set
-    const restaurantsCol = firestore.collection(collection)
-        .doc(document).collection(subcollection).doc(docUID).set(
-      { deleted_at: firebase.firestore.FieldValue.serverTimestamp(),
-      }, { merge: true });
+    // TODO: Validate the object exists before making the set
+    const restaurantsCol = firestore
+      .collection(collection)
+      .doc(document)
+      .collection(subcollection)
+      .doc(docUID)
+      .set(
+        { deleted_at: firebase.firestore.FieldValue.serverTimestamp() },
+        { merge: true },
+      );
 
-    restaurantsCol.then(() => {
-          callback({ uid });
-        }).catch(error => {
-          console.error('Error writing document: ', error);
-        });
+    restaurantsCol
+      .then(() => {
+        callback({ uid });
+      })
+      .catch(error => {
+        console.error('Error writing document: ', error);
+      });
   },
   setDocumentToSubCollection(
-    collection, document, docUID, subcollection, value, callback, _error) {
+    collection,
+    document,
+    docUID,
+    subcollection,
+    value,
+    callback,
+    _error,
+  ) {
     const uid = uuid();
-    const restaurantsCol = firestore.collection(collection)
-    .doc(document).collection(subcollection).doc(docUID).set(
-      { ...value, id: docUID }, { merge: true });
+    const restaurantsCol = firestore
+      .collection(collection)
+      .doc(document)
+      .collection(subcollection)
+      .doc(docUID)
+      .set({ ...value, id: docUID }, { merge: true });
 
-    restaurantsCol.then(() => {
-      callback({ uid, data: { id: docUID, ...value } });
-    }).catch(error => {
-      console.error('Error writing document: ', error);
-    });
+    restaurantsCol
+      .then(() => {
+        callback({ uid, data: { id: docUID, ...value } });
+      })
+      .catch(error => {
+        console.error('Error writing document: ', error);
+      });
   },
   unsubscribe(uid) {
     const listener = subscribers.find(i => i.uid === uid);
